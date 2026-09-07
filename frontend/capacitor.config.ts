b667e11d20c4c4530d0f5b90f08b90446a37c32a
Shivam Kumar Yadav <shivamkumaryadavaug11@gmail.com>
2026-09-07 20:51:51 +0530

    Add initial Capacitor configuration file

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.blinkchat.app',
+  appName: 'BlinkChat',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
