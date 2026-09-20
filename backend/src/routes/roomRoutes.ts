0e23fcfc07410a8198c0b2da12f20ec4548f7f67
Shivam Kumar Yadav <shivamkumaryadavaug11@gmail.com>
2026-09-20 16:51:15 +0530

    Delete backend directory

--- backend/src/routes/roomRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createRoom, checkRoom } from "../controllers/roomController";
-import { validate, createRoomSchema, checkRoomSchema } from "../middleware/validator";
-import { joinRateLimiter } from "../middleware/rateLimiter";
-
-export const roomRoutes = Router();
-
-roomRoutes.post("/rooms", createRoom);
-roomRoutes.post("/rooms/check", joinRateLimiter, checkRoom);
